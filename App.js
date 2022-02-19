5b1544d54147e45d2372102dd0f3069ca4951c15
maz-z <67305715+maz-z@users.noreply.github.com>
2022-02-19 15:39:48 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+// import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Wtf</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+
+
+export default App;
